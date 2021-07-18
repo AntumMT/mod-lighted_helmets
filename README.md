5ba6ffc767bc045834b30f13f7e59e53a050f25f
Jordan Irwin <antumdeluge@gmail.com>
2021-07-17 18:48:53 -0700

    Add setting to exclude helmets

--- README.md
+++ README.md
@@ -5,11 +5,11 @@
 Light-emitting helmets.
 
 Supports helmets from the following mods:
-- 3d_armor
-- technic_armor
-- xtraarmor
-- amber
-- rainbow_ore
+- [3d_armor](https://content.minetest.net/packages/stu/3d_armor/)
+- [technic_armor](https://github.com/stujones11/technic_armor)
+- [xtraarmor](https://forum.minetest.net/viewtopic.php?t=16645)
+- [amber](https://forum.minetest.net/viewtopic.php?t=18186)
+- [rainbow_ore](https://forum.minetest.net/viewtopic.php?id=13519)
 
 ### Licensing:
 
@@ -36,18 +36,25 @@ Supports helmets from the following mods:
 
 ### Requirements:
 
+```
 Minimum Minetest verserion: 0.4
 
 Depends:
-- [3d_armor][]
-- [3d_armor_light](https://content.minetest.net/packages/AntumDeluge/3d_armor_light/)
+- 3d_armor
+- 3d_armor_light
 
 Optional depends:
-- [default](https://github.com/minetest/minetest_game/blob/master/mods/default)
-- [technic_armor](https://github.com/stujones11/technic_armor)
-- [xtraarmor](https://forum.minetest.net/viewtopic.php?t=16645)
-- [amber](https://forum.minetest.net/viewtopic.php?t=18186)
-- [rainbow_ore](https://forum.minetest.net/viewtopic.php?id=13519)
+- default
+- technic_armor
+- xtraarmor
+- amber
+- rainbow_ore
+
+Settings:
+- lighted_helmets.exclude
+  - List of mods or helmets to exclude.
+	- Example: lighted_helmets.exclude = xtraarmor,amber:helmet
+```
 
 ### Links:
 
@@ -55,6 +62,3 @@ Optional depends:
 - [Git repo](https://github.com/AntumMT/mod-lighted_helmets)
 - [Changelog](changelog.txt)
 - [TODO](TODO.txt)
-
-
-[3d_armor]: https://content.minetest.net/packages/stu/3d_armor/
--- init.lua
+++ init.lua
@@ -1,4 +1,17 @@
 
+local excluded = {}
+
+local to_exclude = core.settings:get("lighted_helmets.exclude")
+if to_exclude then
+	if to_exclude:find(",") then
+		for _, tmp in ipairs(to_exclude:split(",")) do
+			excluded[tmp:trim()] = true
+		end
+	else
+		excluded[to_exclude:trim()] = true
+	end
+end
+
 if core.global_exists("armor") then
 	local helmets = {
 		["3d_armor"] = {
@@ -52,65 +65,69 @@ if core.global_exists("armor") then
 	}
 
 	for modname, materials in pairs(helmets) do
-		for _, material in ipairs(materials) do
-			local lvalue
-			if type(material) == "table" then
-				lvalue = material[2]
-				material = material[1]
-			end
-
-			local orig_name
-			if modname == "amber" and material == "amber" then
-				orig_name = modname .. ":helmet"
-			elseif modname == "rainbow_ore" then
-				orig_name = modname .. ":rainbow_ore_helmet"
-			else
-				orig_name = modname .. ":helmet_" .. material
-			end
-
-			local orig_def = core.registered_items[orig_name]
-			if orig_def then
-				local def = table.copy(orig_def)
-
-				def.description = def.description .. " with light"
-				def.inventory_image = "lighted_helmets_inv_underlay.png^" .. orig_def.inventory_image
-				if not def.texture then
-					if modname == "rainbow_ore" then
-						def.texture = "rainbow_ore_rainbow_ore_helmet.png"
-					else
-						def.texture = modname .. "_helmet_" .. material .. ".png"
-					end
-				end
-				if not def.wield_image then
-					def.wield_image = orig_def.inventory_image
-				end
-				if not def.preview then
-					if modname == "rainbow_ore" then
-						def.preview = "rainbow_ore_rainbow_ore_helmet_preview.png"
-					else
-						def.preview = modname .. "_helmet_" .. material .. "_preview.png"
-					end
+		if not excluded[modname] then
+			for _, material in ipairs(materials) do
+				local lvalue
+				if type(material) == "table" then
+					lvalue = material[2]
+					material = material[1]
 				end
 
-				local helmet_name = "lighted_helmets:"
-				if modname == "amber" and material == "ancient" then
-					helmet_name = helmet_name .. "amber_ancient"
+				local orig_name
+				if modname == "amber" and material == "amber" then
+					orig_name = modname .. ":helmet"
+				elseif modname == "rainbow_ore" then
+					orig_name = modname .. ":rainbow_ore_helmet"
 				else
-					helmet_name = helmet_name .. material
+					orig_name = modname .. ":helmet_" .. material
 				end
 
-				armor:register_armor(":" .. helmet_name, def)
-				armor_light.register(helmet_name, lvalue)
+				if not excluded[orig_name] then
+					local orig_def = core.registered_items[orig_name]
+					if orig_def then
+						local def = table.copy(orig_def)
 
-				if core.registered_items["default:mese_crystal"] and core.registered_items["default:torch"] then
-					core.register_craft({
-						output = helmet_name,
-						recipe = {
-							{"default:torch"},
-							{"default:mese_crystal"},
-							{orig_name},
-						},
-					})
+						def.description = def.description .. " with light"
+						def.inventory_image = "lighted_helmets_inv_underlay.png^" .. orig_def.inventory_image
+						if not def.texture then
+							if modname == "rainbow_ore" then
+								def.texture = "rainbow_ore_rainbow_ore_helmet.png"
+							else
+								def.texture = modname .. "_helmet_" .. material .. ".png"
+							end
+						end
+						if not def.wield_image then
+							def.wield_image = orig_def.inventory_image
+						end
+						if not def.preview then
+							if modname == "rainbow_ore" then
+								def.preview = "rainbow_ore_rainbow_ore_helmet_preview.png"
+							else
+								def.preview = modname .. "_helmet_" .. material .. "_preview.png"
+							end
+						end
+
+						local helmet_name = "lighted_helmets:"
+						if modname == "amber" and material == "ancient" then
+							helmet_name = helmet_name .. "amber_ancient"
+						else
+							helmet_name = helmet_name .. material
+						end
+
+						armor:register_armor(":" .. helmet_name, def)
+						armor_light.register(helmet_name, lvalue)
+
+						if core.registered_items["default:mese_crystal"] and core.registered_items["default:torch"] then
+							core.register_craft({
+								output = helmet_name,
+								recipe = {
+									{"default:torch"},
+									{"default:mese_crystal"},
+									{orig_name},
+								},
+							})
+						end
+					end
 				end
 			end
 		end
